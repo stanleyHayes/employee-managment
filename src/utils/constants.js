668924c26f475bbb08f0d819bfc2f5a2fdb26d41
Stanley Hayford <hayfordstanley@gmail.com>
2022-06-21 21:36:57 +0100

    Change description

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:8000/api/v1';
+// const BASE_URL = 'http://localhost:8000/api/v1';
+const BASE_URL = 'https://employees-oyster.herokuapp.com/api/v1';
 
 export const CONSTANTS = {BASE_URL};
